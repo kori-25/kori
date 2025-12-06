05612f67b0e0945f047eb2749423a13d544e6dad
gianlucaporfirio <kori25.news@gmail.com>
2025-12-06 05:28:41 +0100

    051225

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "869948372"
+    serviceWorkerVersion: "256050763"
   }
 });
