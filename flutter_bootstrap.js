559103d3bfb17447ab0d06777954252b8ceab63d
Gianluca Porfirio <79756543+gianporfi@users.noreply.github.com>
2026-09-06 10:26:52 +0200

    060926

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "214623251"
+    serviceWorkerVersion: "2533080808"
   }
 });
